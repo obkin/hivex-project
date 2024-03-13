8e495853715c7fb3f24eb8431ba0eb0e1a03613f
obkin <kapellwork@gmail.com>
2024-03-13 18:35:44 +0200

    added: dependencies

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5050;
 
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalInterceptors(new TransformInterceptor()); // add this thing
   app.enableCors();
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
---
+++ src/weather/weather.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+import { WeatherController } from './weather.controller';
+
+@Module({
+  providers: [WeatherService],
+  controllers: [WeatherController],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class WeatherModule {}
